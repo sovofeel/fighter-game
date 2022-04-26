df6fc8fd51d2bb7d09ede6b2612aee0b6885f722
sovofeel <sovofeel@gmail.com>
2022-04-26 13:19:45 +0500

    add initial canvas

---
+++ index.js
@@ -0,0 +1,8 @@
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+canvas.width = 1024
+canvas.height = 576
+
+ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+ 
